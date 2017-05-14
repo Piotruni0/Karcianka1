fe24a9ff0155e533b5b965dd613c7ab83a62ab65
Piotr <pituzz.01@gmail.com>
2017-05-14 17:48:51 +0200

    Dodaje sockety

--- www/controllers/GameController.php
+++ www/controllers/GameController.php
@@ -2,11 +2,34 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\filters\AccessControl;
+
 class GameController extends \yii\web\Controller
 {
+    public function behaviors()
+    {
+      return [
+          'access' => [
+              'class' => AccessControl::className(),
+              'rules' => [
+                  [
+                      'allow' => true,
+                      'roles' => ['@'],
+                  ],
+              ],
+          ],
+      ];
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionPlay()
+    {
+      echo 'username: ' . Yii::$app->user->identity->username;
     }
 
     public function actionLogin()
--- www/views/game/index.php
+++ www/views/game/index.php
@@ -1,8 +1,13 @@
 <?php
 /* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
 ?>
 <h1>game/index</h1>
 
+
+<?=Html::a(Yii::t('app', 'Graj'), ['game/play'], ['class'=>'btn btn-success']);?>
 <p>
     You may change the content of this page by modifying
     the file <code><?= __FILE__; ?></code>.
